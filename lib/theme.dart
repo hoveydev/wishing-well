0a0354f179ab238880c69dfdcc7f56882e717035
Robert Hovey <robert.hovey95@gmail.com>
2025-09-26 07:31:48 -0400

    flutter setup

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF3A8FB7);
+  static const Color accent = Color(0xFFFFD166);
+  static const Color background = Color(0xFFFAFAFA);
+  static const Color text = Color(0xFF2E2E2E);
+}

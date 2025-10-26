1bc8255d080ad4c474d0d983d596c1c181bb9739
Robert Hovey <robert.hovey95@gmail.com>
2025-10-26 17:13:10 -0400

    login formatting updates

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,80 +4,101 @@ import 'package:wishing_well/components/button/app_button_type.dart';
 import 'package:wishing_well/components/input/app_input.dart';
 import 'package:wishing_well/components/input/app_input_type.dart';
 import 'package:wishing_well/components/logo/app_logo.dart';
-import 'package:wishing_well/components/spacer/app_spacer.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
-        body: SafeArea(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return SingleChildScrollView(
-                padding: const EdgeInsets.all(16),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                  child: IntrinsicHeight(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(
-                          children: [
-                            AppLogo(size: 150),
-                            AppSpacer.small(),
-                            Text("WishingWell", style: textTheme.headlineLarge),
-                            AppSpacer.xsmall(),
-                            Text(
-                              "Your personal well for thoughtful giving",
-                              style: textTheme.bodyMedium,
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            AppInput(
-                              placeholder: "Email",
-                              type: AppInputType.text,
-                              controller: TextEditingController(),
-                            ),
-                            AppSpacer.medium(),
-                            AppInput(
-                              placeholder: "Password",
-                              type: AppInputType.text,
-                              controller: TextEditingController(),
-                            ),
-                            AppSpacer.large(),
-                            AppButton(
-                              label: "Forgot Password?",
-                              onPressed: () {},
-                              type: AppButtonType.tertiary,
-                            ),
-                            AppSpacer.medium(),
-                            AppButton(
-                              label: 'Sign in',
-                              onPressed: () {},
-                              type: AppButtonType.primary,
-                            ),
-                            AppSpacer.medium(),
-                            AppButton(
-                              label: 'Create an Account',
-                              onPressed: () {},
-                              type: AppButtonType.secondary,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: SizedBox(
+                height: constraints.maxHeight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildHeader(context, constraints.maxHeight),
+                    _buildInputSection(context),
+                    _buildButtonsSection(context),
+                  ],
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
-      );
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context, double height) {
+
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final logoSize = height * 0.15; // TODO: this should be held in a constant somewhere
+
+    return Column(
+      spacing: 8,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AppLogo(size: logoSize),
+        Text(
+          "WishingWell",
+          style: textTheme.headlineLarge,
+          textAlign: TextAlign.center
+        ),
+        Text(
+          "Your personal well for thoughtful giving",
+          style: textTheme.bodyMedium,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+    
+  }
+
+  Widget _buildInputSection(BuildContext context) {
+    return Column(
+      spacing: 8,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AppInput(
+          placeholder: "Email",
+          type: AppInputType.text,
+          controller: TextEditingController(),
+        ),
+        AppInput(
+          placeholder: "Password",
+          type: AppInputType.text,
+          controller: TextEditingController(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButtonsSection(BuildContext context) {
+    return Column(
+      spacing: 8,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AppButton(
+          label: "Forgot Password?",
+          onPressed: () {},
+          type: AppButtonType.tertiary,
+        ),
+        AppButton(
+          label: 'Sign in',
+          onPressed: () {},
+          type: AppButtonType.primary,
+        ),
+        AppButton(
+          label: 'Create an Account',
+          onPressed: () {},
+          type: AppButtonType.secondary,
+        ),
+      ],
+    );
   }
 }

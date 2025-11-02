c30336c01d56ea23330d738d8ff87995f03109a1
Robert Hovey <robert.hovey95@gmail.com>
2025-11-02 12:03:31 -0500

    fixed AppLogoSize logic

--- lib/theme/app_logo_size.dart
+++ lib/theme/app_logo_size.dart
@@ -4,10 +4,10 @@ class AppLogoSize {
   const AppLogoSize({this.sectionHeight});
 
   static const double _large = 60.0;
-  static const double _headerLargeRatio = 0.15;
+  static const double _headerLargeRatio = 0.12;
 
   double get large {
-    if (sectionHeight != null) return _large;
+    if (sectionHeight == null) return _large;
     return sectionHeight! * _headerLargeRatio;
   }
 }

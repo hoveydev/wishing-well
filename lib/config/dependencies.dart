4619f337fa28c0ad004a35b271e6da116081c673
Robert Hovey <robert.hovey95@gmail.com>
2025-12-02 22:33:23 -0500

    fixed dependencies to get app working

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,9 +1,15 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:wishing_well/app_config.dart';
 import 'package:wishing_well/data/respositories/auth/auth_repository.dart';
 import 'package:wishing_well/data/respositories/auth/auth_repository_remote.dart';
 
 List<SingleChildWidget> get providersRemote => [
+  Provider(
+    create: (context) =>
+        SupabaseClient(AppConfig.supabaseUrl, AppConfig.supabaseSecret),
+  ),
   ChangeNotifierProvider(
     create: (context) =>
         AuthRepositoryRemote(supabase: context.read()) as AuthRepository,
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:wishing_well/app_config.dart';
 import 'package:wishing_well/config/dependencies.dart';
 import 'package:wishing_well/l10n/app_localizations.dart';
@@ -11,11 +10,12 @@ import 'package:wishing_well/theme/app_theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppConfig.initialize();
-  await Supabase.initialize(
-    url: AppConfig.supabaseUrl,
-    anonKey: AppConfig.supabaseSecret,
+  runApp(
+    MultiProvider(
+      providers: providersRemote,
+      builder: (context, child) => const MainApp(),
+    ),
   );
-  runApp(MultiProvider(providers: providersRemote, child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {

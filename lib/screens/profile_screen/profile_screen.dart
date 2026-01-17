3fff61f554f82683e4a0a3d0e4d632303ee9bd71
Robert Hovey <robert.hovey95@gmail.com>
2026-01-17 00:42:35 -0500

    added close on app bar

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wishing_well/components/app_bar/app_menu_bar.dart';
+import 'package:wishing_well/components/app_bar/app_menu_bar_type.dart';
 import 'package:wishing_well/components/screen/screen.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const Screen(children: []);
+  Widget build(BuildContext context) => Screen(
+    appBar: AppMenuBar(action: () => context.pop(), type: AppMenuBarType.close),
+    children: const [],
+  );
 }

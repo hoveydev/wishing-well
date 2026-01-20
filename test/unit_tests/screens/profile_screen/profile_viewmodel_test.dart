ff665cce077999d80d4c1ccd18c3d416feb78cce
Robert Hovey <robert.hovey95@gmail.com>
2026-01-20 08:39:48 -0500

    used correct mcok auth repo for profile viewmodel tests

--- test/unit_tests/screens/profile_screen/profile_viewmodel_test.dart
+++ test/unit_tests/screens/profile_screen/profile_viewmodel_test.dart
@@ -1,15 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:wishing_well/data/respositories/auth/auth_repository.dart';
 import 'package:wishing_well/screens/profile_screen/profile_viewmodel.dart';
 import 'package:wishing_well/utils/auth_error.dart';
 import 'package:wishing_well/utils/result.dart';
 
+import '../../../../testing_resources/mocks/repositories/mock_auth_repository.dart';
+
 void main() {
-  late _TestAuthRepository authRepository;
+  late MockAuthRepository authRepository;
   late ProfileViewModel viewModel;
 
   setUp(() {
-    authRepository = _TestAuthRepository(logoutResult: const Result.ok(null));
+    authRepository = MockAuthRepository(logoutResult: const Result.ok(null));
     viewModel = ProfileViewModel(authRepository: authRepository);
   });
 
@@ -29,43 +30,4 @@ void main() {
       expect(viewModel.hasAlert, false);
     });
   });
-}
-
-class _TestAuthRepository extends AuthRepository {
-  _TestAuthRepository({required this.logoutResult});
-
-  final Result<void> logoutResult;
-
-  @override
-  bool get isAuthenticated => true;
-
-  @override
-  String? get userFirstName => null;
-
-  @override
-  Future<Result<void>> login({
-    required String email,
-    required String password,
-  }) async => const Result.ok(null);
-
-  @override
-  Future<Result<void>> logout() async => logoutResult;
-
-  @override
-  Future<Result<void>> createAccount({
-    required String email,
-    required String password,
-  }) async => const Result.ok(null);
-
-  @override
-  Future<Result<void>> sendPasswordResetRequest({
-    required String email,
-  }) async => const Result.ok(null);
-
-  @override
-  Future<Result<void>> resetUserPassword({
-    required String email,
-    required String newPassword,
-    required String token,
-  }) async => const Result.ok(null);
 }

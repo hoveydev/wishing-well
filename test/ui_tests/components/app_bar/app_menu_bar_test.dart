1a721bb01b5968404a20ba9883f620c77f83822d
Robert Hovey <robert.hovey95@gmail.com>
2026-02-01 22:00:52 -0500

    app manu bar testing updates

--- test/ui_tests/components/app_bar/app_menu_bar_test.dart
+++ test/ui_tests/components/app_bar/app_menu_bar_test.dart
@@ -6,9 +6,11 @@ import 'package:wishing_well/components/app_bar/app_menu_bar_type.dart';
 import 'package:wishing_well/l10n/app_localizations.dart';
 import 'package:wishing_well/theme/app_theme.dart';
 
+import '../../../../testing_resources/helpers/test_helpers.dart';
+
 void main() {
   group('AppMenuBar', () {
-    Widget createTestWidget(AppMenuBar menuBar) => MaterialApp(
+    Widget createTestWidget(Widget child) => MaterialApp(
       theme: AppTheme.lightTheme,
       localizationsDelegates: const [
         AppLocalizations.delegate,
@@ -17,114 +19,168 @@ void main() {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: Scaffold(appBar: menuBar),
+      home: child,
     );
 
-    group('Main Menu Bar', () {
-      testWidgets('renders with title and account icon', (tester) async {
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(type: AppMenuBarType.main, action: () {}),
-          ),
-        );
-
-        expect(find.text('WishingWell'), findsOneWidget);
-        expect(find.byIcon(Icons.account_circle), findsOneWidget);
-      });
-
-      testWidgets('calls action when account button tapped', (tester) async {
-        bool actionCalled = false;
-
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(
-              type: AppMenuBarType.main,
-              action: () {
-                actionCalled = true;
-              },
+    group(TestGroups.rendering, () {
+      group('Main Menu Bar', () {
+        testWidgets('renders with title and account icon', (
+          WidgetTester tester,
+        ) async {
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(type: AppMenuBarType.main, action: () {}),
+              ),
             ),
-          ),
-        );
+          );
+          await TestHelpers.pumpAndSettle(tester);
 
-        await tester.tap(find.byIcon(Icons.account_circle));
-        expect(actionCalled, true);
+          TestHelpers.expectTextOnce('WishingWell');
+          expect(find.byIcon(Icons.account_circle), findsOneWidget);
+        });
+
+        testWidgets('renders logo in leading position', (
+          WidgetTester tester,
+        ) async {
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(type: AppMenuBarType.main, action: () {}),
+              ),
+            ),
+          );
+          await TestHelpers.pumpAndSettle(tester);
+
+          expect(find.byType(FittedBox), findsWidgets);
+        });
       });
 
-      testWidgets('renders logo in leading position', (tester) async {
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(type: AppMenuBarType.main, action: () {}),
-          ),
-        );
+      group('Close Menu Bar', () {
+        testWidgets('renders with close icon in actions', (
+          WidgetTester tester,
+        ) async {
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(type: AppMenuBarType.close, action: () {}),
+              ),
+            ),
+          );
+          await TestHelpers.pumpAndSettle(tester);
 
-        expect(find.byType(FittedBox), findsWidgets);
+          expect(find.byIcon(Icons.close), findsOneWidget);
+          expect(find.text('WishingWell'), findsNothing);
+        });
+      });
+
+      group('Dismiss Menu Bar', () {
+        testWidgets('renders with dismiss icon in leading', (
+          WidgetTester tester,
+        ) async {
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(type: AppMenuBarType.dismiss, action: () {}),
+              ),
+            ),
+          );
+          await TestHelpers.pumpAndSettle(tester);
+
+          expect(find.byIcon(Icons.keyboard_arrow_down), findsOneWidget);
+          expect(find.text('WishingWell'), findsNothing);
+        });
       });
     });
 
-    group('Close Menu Bar', () {
-      testWidgets('renders with close icon in actions', (tester) async {
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(type: AppMenuBarType.close, action: () {}),
-          ),
-        );
+    group(TestGroups.interaction, () {
+      group('Main Menu Bar', () {
+        testWidgets('calls action when account button is tapped', (
+          WidgetTester tester,
+        ) async {
+          bool actionCalled = false;
 
-        expect(find.byIcon(Icons.close), findsOneWidget);
-        expect(find.text('WishingWell'), findsNothing);
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(
+                  type: AppMenuBarType.main,
+                  action: () {
+                    actionCalled = true;
+                  },
+                ),
+              ),
+            ),
+          );
+          await TestHelpers.pumpAndSettle(tester);
+
+          await TestHelpers.tapAndSettle(
+            tester,
+            find.byIcon(Icons.account_circle),
+          );
+
+          expect(actionCalled, isTrue);
+        });
       });
 
-      testWidgets('calls action when close button tapped', (tester) async {
-        bool actionCalled = false;
+      group('Close Menu Bar', () {
+        testWidgets('calls action when close button is tapped', (
+          WidgetTester tester,
+        ) async {
+          bool actionCalled = false;
 
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(
-              type: AppMenuBarType.close,
-              action: () {
-                actionCalled = true;
-              },
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(
+                  type: AppMenuBarType.close,
+                  action: () {
+                    actionCalled = true;
+                  },
+                ),
+              ),
             ),
-          ),
-        );
+          );
+          await TestHelpers.pumpAndSettle(tester);
 
-        await tester.tap(find.byIcon(Icons.close));
-        expect(actionCalled, true);
+          await TestHelpers.tapAndSettle(tester, find.byIcon(Icons.close));
+
+          expect(actionCalled, isTrue);
+        });
+      });
+
+      group('Dismiss Menu Bar', () {
+        testWidgets('calls action when dismiss button is tapped', (
+          WidgetTester tester,
+        ) async {
+          bool actionCalled = false;
+
+          await tester.pumpWidget(
+            createTestWidget(
+              Scaffold(
+                appBar: AppMenuBar(
+                  type: AppMenuBarType.dismiss,
+                  action: () {
+                    actionCalled = true;
+                  },
+                ),
+              ),
+            ),
+          );
+          await TestHelpers.pumpAndSettle(tester);
+
+          await TestHelpers.tapAndSettle(
+            tester,
+            find.byIcon(Icons.keyboard_arrow_down),
+          );
+
+          expect(actionCalled, isTrue);
+        });
       });
     });
 
-    group('Dismiss Menu Bar', () {
-      testWidgets('renders with dismiss icon in leading', (tester) async {
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(type: AppMenuBarType.dismiss, action: () {}),
-          ),
-        );
-
-        expect(find.byIcon(Icons.keyboard_arrow_down), findsOneWidget);
-        expect(find.text('WishingWell'), findsNothing);
-      });
-
-      testWidgets('calls action when dismiss button tapped', (tester) async {
-        bool actionCalled = false;
-
-        await tester.pumpWidget(
-          createTestWidget(
-            AppMenuBar(
-              type: AppMenuBarType.dismiss,
-              action: () {
-                actionCalled = true;
-              },
-            ),
-          ),
-        );
-
-        await tester.tap(find.byIcon(Icons.keyboard_arrow_down));
-        expect(actionCalled, true);
-      });
-    });
-
-    group('PreferredSize', () {
-      testWidgets('has correct preferred height', (tester) async {
+    group(TestGroups.behavior, () {
+      testWidgets('has correct preferred height', (WidgetTester tester) async {
         final menuBar = AppMenuBar(type: AppMenuBarType.main, action: () {});
 
         expect(menuBar.preferredSize.height, 48);

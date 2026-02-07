dd92fce318f3e498fcfc5868be56c5c86311e0f6
Robert Hovey <robert.hovey95@gmail.com>
2026-02-07 18:00:02 -0500

    removed header on home for now

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,10 +3,8 @@ import 'package:go_router/go_router.dart';
 import 'package:wishing_well/components/app_bar/app_menu_bar.dart';
 import 'package:wishing_well/components/app_bar/app_menu_bar_type.dart';
 import 'package:wishing_well/components/screen/screen.dart';
-import 'package:wishing_well/components/spacer/app_spacer.dart';
 import 'package:wishing_well/components/wishers/wishers_list.dart';
 import 'package:wishing_well/routing/routes.dart';
-import 'package:wishing_well/screens/home/components/home_header.dart';
 import 'package:wishing_well/screens/home/home_view_model.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -31,8 +29,6 @@ class _HomeScreenState extends State<HomeScreen> {
     ),
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      HomeHeader(firstName: widget.viewModel.firstName),
-      const AppSpacer.large(),
       WishersList(
         onAddWisherTap: () => context.pushNamed(Routes.addWisher.name),
       ),
